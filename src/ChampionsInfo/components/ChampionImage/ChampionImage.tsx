e97ecdc138cd47fa80b5d48a4af08e817b49a10b
Afergon <adrian.afergon@gmail.com>
2019-07-07 12:35:29 +0100

    [Emerged-Design]: Add functionality.

--- src/ChampionsInfo/components/ChampionImage/ChampionImage.tsx
+++ src/ChampionsInfo/components/ChampionImage/ChampionImage.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import './ChampionImage.scss';
+import { Image } from '../Image/';
 
-type ChampionImageProps = {name: string, src: string}
+type ChampionImageProps = {id?:string, name: string, src: string, onClick?: (id: string) => void}
 
-export const ChampionImage: React.FC<ChampionImageProps> = ({name, src}) => (
-  <div className="ChampionImage">
-    Hello from ChampionImage!
-  </div>
-);
+export const ChampionImage: React.FC<ChampionImageProps> = ({id, name, src, onClick}) => {
+
+    const handleClick = () => {
+        if(onClick && id) {
+            onClick(id)
+        }
+    };
+
+    return (
+    <div onClick={handleClick}>
+        <Image src={src} alt={name} data-test-id="image"/>
+    </div>
+)};
 
 ChampionImage.displayName = 'ChampionImage';
