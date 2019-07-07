a5616b79bf4746766db8e03a7ffbf4678c33b43c
Afergon <adrian.afergon@gmail.com>
2019-07-07 16:03:27 +0100

    [Emerged-Design]: Iterate by champions to display each image (green).

--- src/ChampionsInfo/components/ChampionSelector/ChampionSelector.tsx
+++ src/ChampionsInfo/components/ChampionSelector/ChampionSelector.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import './ChampionSelector.scss';
 import {Champion} from "../ChampionImage/ChampionImage.spec";
+import {ChampionImage} from "../ChampionImage";
 
 interface ChampionSelectorProps {
     champions: Champion[];
 }
 
-export const ChampionSelector: React.FC<ChampionSelectorProps> = () => (
-  <div className="ChampionSelector">
-    Hello from ChampionSelector!
-  </div>
-);
+export const ChampionSelector: React.FC<ChampionSelectorProps> = ({champions}) =>
+    champions.map((champion) => <ChampionImage champion={champion} />);
 
 ChampionSelector.displayName = 'ChampionSelector';
