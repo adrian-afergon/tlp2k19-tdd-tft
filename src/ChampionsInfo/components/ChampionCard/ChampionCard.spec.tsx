19798e1c67fd688a0558f4349647bf63604742e6
Afergon <adrian.afergon@gmail.com>
2019-07-07 18:58:13 +0100

    [Emerged-Design]: Add test for check the champion properties are been displayed.

--- src/ChampionsInfo/components/ChampionCard/ChampionCard.spec.tsx
+++ src/ChampionsInfo/components/ChampionCard/ChampionCard.spec.tsx
@@ -1,16 +1,35 @@
-import { shallow } from 'enzyme';
+import {shallow, ShallowWrapper} from 'enzyme';
 import * as React from 'react';
 import { ChampionCard} from './';
 import {Champion} from '../../models/Champion';
 
 describe('ChampionCard', () => {
-  it('should display the component', () => {
-    const aChampion: Champion = {
-      name: 'irrelevant name',
-      image: 'irrelevant image',
-      id: 'irrelevant id',
-    };
-    const wrapper = shallow(<ChampionCard champion={aChampion} />);
-    expect(wrapper.exists()).toBeTruthy();
+
+  const aChampion: Champion = {
+    name: 'irrelevant name',
+    image: 'irrelevant image',
+    id: 'irrelevant id',
+  };
+
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ChampionCard champion={aChampion} />);
   });
+
+  it('should display the name', () => {
+    const name = wrapper.find('[data-test-id="name"]');
+    expect(name.text()).toEqual(aChampion.name);
+  });
+
+  it('should display the image', () => {
+    const image = wrapper.find('[data-test-id="image"]');
+    expect(image.exists()).toBeTruthy();
+  });
+
+  it('should display the description', () => {
+    const info = wrapper.find('[data-test-id="info"]');
+    expect(info.text()).toBe(aChampion.info);
+  });
+
 });
