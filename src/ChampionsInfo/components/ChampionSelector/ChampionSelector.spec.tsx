74b29b447818abd78879f243a813e77b50c61a73
Afergon <adrian.afergon@gmail.com>
2019-07-07 15:47:24 +0100

    [Emerged-Design]: Define a test for check the display of multiple champions (red).

--- src/ChampionsInfo/components/ChampionSelector/ChampionSelector.spec.tsx
+++ src/ChampionsInfo/components/ChampionSelector/ChampionSelector.spec.tsx
@@ -4,11 +4,20 @@ import { ChampionSelector} from './';
 import {Champion} from "../ChampionImage/ChampionImage.spec";
 
 describe('ChampionSelector', ()=> {
+
   it('should display the champions', ()=>{
-    const someChampions: Champion[] = [];
+    const aatrox: Champion = buildChampion({name: 'Aatrox'} as Champion);
+    const kayle: Champion = buildChampion({name: 'Kayle'} as Champion);
+    const fiora: Champion = buildChampion({name: 'Fiora'} as Champion);
+    const someChampions: Champion[] = [aatrox, kayle, fiora];
     const wrapper = shallow(<ChampionSelector champions={someChampions}/>);
     const championImages = wrapper.find('ChampionImage');
     expect(championImages.length).toBe(someChampions.length);
   })
 });
 
+const buildChampion = ({id, name, image}: Champion):Champion => ({
+  id: id ? id : 'irrelevant id',
+  name: name ? name : 'irrelevant name',
+  image: image ? image : 'irrelevant image'
+});
