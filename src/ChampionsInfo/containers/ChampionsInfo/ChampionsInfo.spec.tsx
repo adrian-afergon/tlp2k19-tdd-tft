0e6be5226b29983ce9985ffb0989d442f740d7e2
Afergon <adrian.afergon@gmail.com>
2019-07-12 08:46:42 +0100

    [BlackBox]: Implement a sample test, that it not works with useEffect hook.

--- src/ChampionsInfo/containers/ChampionsInfo/ChampionsInfo.spec.tsx
+++ src/ChampionsInfo/containers/ChampionsInfo/ChampionsInfo.spec.tsx
@@ -1,10 +1,17 @@
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 import * as React from 'react';
 import { ChampionsInfo} from './';
 
 describe('ChampionsInfo', () => {
-  it('should load a champions list when the component is displayed', () => {
-    const wrapper = mount(<ChampionsInfo />);
+  it('should load a champions list when the component is displayed', async (done) => {
+    const aLoadMethod = jest.fn( async () => {done(); return 'Hello'; });
+
+    const wrapper = mount(<ChampionsInfo methodTest={aLoadMethod}/>);
+    wrapper.update();
+    const message = wrapper.find('[data-test-id="message"]');
+
+    expect(aLoadMethod).toHaveBeenCalled();
+    expect(message).toEqual('Hello');
     expect(wrapper.exists()).toBeTruthy();
   });
 });
--- src/ChampionsInfo/containers/ChampionsInfo/ChampionsInfo.tsx
+++ src/ChampionsInfo/containers/ChampionsInfo/ChampionsInfo.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import './ChampionsInfo.scss';
+import {useEffect, useState} from 'react';
 
-export const ChampionsInfo: React.FC<{}> = () => (
-  <div className="ChampionsInfo">
-    Hello from ChampionsInfo!
-  </div>
-);
+export const ChampionsInfo: React.FC<{methodTest: () => Promise<string> }> = ({methodTest}) => {
+
+    const [hello, setHello] = useState('');
+    const sayHello = async () => {
+        setHello(await methodTest());
+    };
+
+    useEffect(() => {
+        sayHello().then(() => {});
+    }, []);
+
+    return (
+    <div className="ChampionsInfo">
+      <p data-test-id="message">{hello}</p>
+    </div>
+  );
+};
 
 ChampionsInfo.displayName = 'ChampionsInfo';
